67a637aef9fa4cb8418ea3e690bc5cb7f1691c8e
zwados <lukasz.rozwadowski7@gmail.com>
2022-10-03 16:10:22 +0200

    deploy search by from_date and to_date functionality & add searching by multiple categories

--- expenses/forms.py
+++ expenses/forms.py
@@ -1,11 +1,18 @@
 from django import forms
-from .models import Expense
+from .models import Expense, Category
 
+# fetching choices from the Category db
+CHOICES = ((category.id, category.name) for category in Category.objects.all())
 
 class ExpenseSearchForm(forms.ModelForm):
+    from_date = forms.DateTimeField(required=False)
+    to_date = forms.DateTimeField(required=False)
+
+    # passing choices to TypedMultipleChoiceField
+    categories = forms.TypedMultipleChoiceField(choices=CHOICES, required=False)
     class Meta:
         model = Expense
-        fields = ('name',)
+        fields = ('name','from_date','to_date', 'categories')
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
--- expenses/views.py
+++ expenses/views.py
@@ -3,6 +3,7 @@ from django.views.generic.list import ListView
 from .forms import ExpenseSearchForm
 from .models import Expense, Category
 from .reports import summary_per_category
+from datetime import date
 
 
 class ExpenseListView(ListView):
@@ -15,8 +16,40 @@ class ExpenseListView(ListView):
         form = ExpenseSearchForm(self.request.GET)
         if form.is_valid():
             name = form.cleaned_data.get('name', '').strip()
-            if name:
-                queryset = queryset.filter(name__icontains=name)
+            # getting today's date
+            today = date.today()
+            # getting values from from_date and to_date in input fields
+            from_date = form['from_date'].value()
+            to_date = form['to_date'].value()
+            categories = form.cleaned_data.get('categories', '')
+            
+            if categories:
+                categories=categories
+                print(categories)
+            else:
+                categories = Category.objects.all()
+
+            # if there is a date chosen - keep it; else select the earliest date in the db
+            if from_date:
+                from_date = from_date
+            else:
+                from_date = Expense.objects.exclude(date__isnull=True).order_by('date').first().date
+                print(from_date)
+
+            # if there is a date chosen - keep it; else select today's date
+            if to_date:
+                to_date = to_date
+            else:
+                to_date = today
+            
+            if name or from_date or to_date:
+                queryset = queryset.filter(
+                    name__icontains=name, date__range=[
+                    from_date,
+                    to_date
+                    ],
+                    category__in = categories
+                )
 
         return super().get_context_data(
             form=form,
