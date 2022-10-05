633892704d3fb71004705aa9261771f96fe2ebce
zwados <lukasz.rozwadowski7@gmail.com>
2022-10-05 21:42:19 +0200

    add bootstrap front-end - app fully functional

--- expenses/forms.py
+++ expenses/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Expense, Category
+from django.forms import widgets
 
 
 class ExpenseSearchForm(forms.ModelForm):
@@ -21,8 +22,17 @@ class ExpenseSearchForm(forms.ModelForm):
     class Meta:
         model = Expense
         fields = ('name','from_date','to_date', 'ordering_by')
+        
 
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+        super(ExpenseSearchForm, self).__init__(*args, **kwargs)
+
         self.fields['name'].required = False
+
         self.fields['categories'] = forms.TypedMultipleChoiceField(choices=((category.id, category.name) for category in Category.objects.all()), required=False)
+
+        self.labels = {'name':'Expense name', "from_date": "Search start date", "to_date": "Search end date", 'ordering_by': "Order search results", "categories": ""}
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'form-control', 'placeholder': f'{self.labels[name]}'})
+
